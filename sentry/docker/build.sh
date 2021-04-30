79a580cba426c6cccfb8930abadf8be39fc9585f
Juan Jimenez-Anca <cortopy@users.noreply.github.com>
2021-04-30 16:52:53 +0100

    further adjustments

--- sentry/docker/build.sh
+++ sentry/docker/build.sh
@@ -4,18 +4,29 @@ set -e
 
 export DOCKER_BUILDKIT=1
 REGISTRY=docker.io/j3asolutions
-SENTRY=$REGISTRY/sentry:21.3.0
-SNUBA=$REGISTRY/snuba:21.3.0
-CLICKHOUSE=$REGISTRY/clickhouse-server:20.8.15.11
+SENTRY_TAG=21.4.1
+SENTRY=$REGISTRY/sentry:$SENTRY_TAG
+SNUBA=$REGISTRY/snuba:$SENTRY_TAG
+CLICKHOUSE_TAG=21.3.8.76
+CLICKHOUSE=$REGISTRY/clickhouse-server:$CLICKHOUSE_TAG
 
-docker build -f Dockerfile.sentry --tag "$SENTRY" .;
+docker build \
+  -f Dockerfile.sentry \
+  --build-arg SENTRY_TAG=$SENTRY_TAG \
+  --tag "$SENTRY" .;
 
 docker push $SENTRY;
 
-docker build -f Dockerfile.snuba --tag "$SNUBA" .;
+docker build \
+  -f Dockerfile.snuba \
+  --build-arg SENTRY_TAG=$SENTRY_TAG \
+  --tag "$SNUBA" .;
 
 docker push $SNUBA;
 
-docker build -f Dockerfile.clickhouse --tag "$CLICKHOUSE" .;
+docker build \
+  -f Dockerfile.clickhouse \
+  --build-arg CLICKHOUSE_TAG=$CLICKHOUSE_TAG \
+  --tag "$CLICKHOUSE" .;
 
 docker push $CLICKHOUSE;
